1fd151e0ff3d10896efb9290d01022086eb3741b
unknown <vkfnska@gmail.com>
2021-12-20 04:50:28 +0900

    폴더 구조 최적화

---
+++ app/app.js
@@ -0,0 +1,16 @@
+// 모듈
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+// 라우팅
+const home = require("./src/routes/home");
+
+// 앱 셋팅
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+// use - > 미들 웨어를 등록해주는 메소드
+app.use("/", home);
+
+module.exports = app;
