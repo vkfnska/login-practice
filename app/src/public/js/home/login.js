d80c8b79156b6d1d49d4331699b71b58af4a3bd1
unknown <vkfnska@gmail.com>
2021-12-20 05:48:35 +0900

    dom에 id,psword 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,12 @@
-console.log("hello");
-console.log("bye");
-console.log("see u");
+const id = document.querySelector("#id"),
+  psword = document.querySelector("#psword"),
+  loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id: id.value,
+        psword: psword.value
+    };
+}
